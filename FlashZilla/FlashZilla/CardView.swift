4e5b6dcc5f9bfc8643362505a548e7dccf7ea689
Niklas <niklas@kizo-agency.com>
2022-12-07 11:46:07 +0100

    Add card view for flashzilla app

---
+++ FlashZilla/FlashZilla/CardView.swift
@@ -0,0 +1,41 @@
+//
+//  CardView.swift
+//  FlashZilla
+//
+//  Created by Niklas Fischer on 7/12/22.
+//
+
+import SwiftUI
+
+struct CardView: View {
+    let card: Card
+    @State private var isShowingAnswer = false
+    
+    var body: some View {
+        ZStack {
+            RoundedRectangle(cornerRadius: 25, style: .continuous)
+                .fill(.white)
+                .shadow(radius: 5)
+            VStack {
+                Text(card.prompt).font(.largeTitle).foregroundColor(.black)
+                if isShowingAnswer {
+                    Text(card.answer)
+                    .font(.title)
+                    .foregroundColor(.gray)
+                }
+            }
+            .padding(40)
+            .multilineTextAlignment(.center)
+        }
+        .frame(width: 450, height: 250)
+        .onTapGesture {
+            isShowingAnswer.toggle()
+        }
+    }
+}
+
+struct CardView_Previews: PreviewProvider {
+    static var previews: some View {
+        CardView(card: Card.example)
+    }
+}
---
+++ FlashZilla/FlashZilla/ContentView.swift
@@ -0,0 +1,40 @@
+//
+//  ContentView.swift
+//  FlashZilla
+//
+//  Created by Niklas Fischer on 7/12/22.
+//
+
+import SwiftUI
+
+extension View {
+    func stacked(at position: Int, in total: Int) -> some View {
+        let offset = Double(total - position)
+        return self.offset(x: 0, y: offset * 10)
+    }
+}
+
+struct ContentView: View {
+    @State private var cards = Array<Card>(repeating: Card.example, count: 10)
+    
+    var body: some View {
+        ZStack {
+            Image("background")
+                .resizable()
+                .ignoresSafeArea()
+            VStack {
+                ZStack {
+                    ForEach(0..<cards.count, id: \.self) { index in
+                        CardView(card: cards[index]).stacked(at: index, in: cards.count)
+                    }
+                }
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
