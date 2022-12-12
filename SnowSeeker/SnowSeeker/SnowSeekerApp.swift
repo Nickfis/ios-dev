8ad0ba78db8e050ecef3d1ad47f244f478e0da1e
Niklas <niklas@kizo-agency.com>
2022-12-12 19:04:47 +0100

    Initial commit snowseeker

---
+++ SnowSeeker/SnowSeeker/SnowSeekerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnowSeekerApp.swift
+//  SnowSeeker
+//
+//  Created by Niklas Fischer on 12/12/22.
+//
+
+import SwiftUI
+
+@main
+struct SnowSeekerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
