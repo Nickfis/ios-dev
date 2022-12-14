382527fa4832e339601c84462ae3c04609802144
Niklas <niklas@kizo-agency.com>
2022-12-14 13:59:13 +0100

    Finalized snowseeker from video

--- SnowSeeker/SnowSeeker/ResortView.swift
+++ SnowSeeker/SnowSeeker/ResortView.swift
@@ -9,6 +9,14 @@ import SwiftUI
 
 struct ResortView: View {
     let resort: Resort
+    @State private var selectedFacility: Facility?
+    @State private var showingFacility = false
+    
+    @Environment(\.horizontalSizeClass) var sizeClass
+    @Environment(\.dynamicTypeSize) var typeSize
+    
+    @EnvironmentObject var favorites: Favorites
+    
     var body: some View {
         ScrollView {
             VStack(alignment: .leading, spacing: 0) {
@@ -17,11 +25,17 @@ struct ResortView: View {
                     .scaledToFit()
                 
                 HStack {
-                    ResortDetailsView(resort: resort)
-                    SkiDetailsView(resort: resort)
+                    if sizeClass == .compact && typeSize > .large {
+                        VStack(spacing: 10) {ResortDetailsView(resort: resort)}
+                        VStack(spacing: 10) {SkiDetailsView(resort: resort)}
+                    } else {
+                        ResortDetailsView(resort: resort)
+                        SkiDetailsView(resort: resort)
+                    }
                 }
                 .padding(.vertical)
                 .background(Color.primary.opacity(0.1))
+                .dynamicTypeSize(...DynamicTypeSize.xxxLarge)
                 
                 
                 Group {
@@ -31,14 +45,36 @@ struct ResortView: View {
                     Text("Facilities")
                         .font(.headline)
                     
-                    Text(resort.facilities, format: .list(type: .and))
-                        .padding(.vertical)
+                    HStack {
+                        ForEach(resort.facilityTypes) { facility in
+                            Button {
+                                selectedFacility = facility
+                                showingFacility = true
+                            } label: {
+                                facility.icon.font(.title)
+                            }
+                        }
+                    }
+                    Button(favorites.contains(resort) ? "Remove from Favorites" : "Add to Favorites") {
+                        if favorites.contains(resort) {
+                            favorites.remove(resort)
+                        } else {
+                            favorites.add(resort)
+                        }
+                    }
+                    .buttonStyle(.borderedProminent)
+                    .padding()
                 }
                 .padding(.horizontal)
             }
         }
         .navigationTitle("\(resort.name), \(resort.country)")
         .navigationBarTitleDisplayMode(.inline)
+        .alert(selectedFacility?.name ?? "More information", isPresented: $showingFacility,
+               presenting: selectedFacility) { _ in
+        } message: {facility in
+            Text(facility.description)
+        }
     }
 }
 
@@ -46,6 +82,6 @@ struct ResortView_Previews: PreviewProvider {
     static var previews: some View {
         NavigationView {
             ResortView(resort: Resort.example)
-        }
+        }.environmentObject(Favorites())
     }
 }
